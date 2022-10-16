8c067b24585d7be50b5f840b9b5ccfd6b8ca07a2
NyanCattEd <36034521+ikarus-999@users.noreply.github.com>
2022-10-16 21:00:33 +0900

    Site updated: 2022-10-16 21:00:31

---
+++ js/gtag.js
@@ -0,0 +1,12 @@
+function google_a4() {
+	return (
+		<>
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-M6KHR911M6"></script><script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments)};
+				gtag('js', new Date());
+				gtag('config', 'G-M6KHR911M6');
+			</script>
+		</>
+	);
+}
